274921e2be5c0f2ead14729786a297dba265675d
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2023-09-19 16:11:43 +0530

    Connected wih mongodb

---
+++ vite-project/Server/Index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+const app=express()
+dotenv.config()
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+const PORT=process.env.PORT
+app.listen(PORT,()=>{
+    console.log(`Server Running on the port ${PORT}  ...`)
+})
+const DB_URL=process.env.CONNECTION_URL
+mongoose.connect(DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    console.log("Mongo Db Database Connected")
+}).catch((error)=>{
+    console.log(error)
+})
+//mongodb+srv://Arnab199993:<password>@cluster0.symytbj.mongodb.net/?retryWrites=true&w=majority
